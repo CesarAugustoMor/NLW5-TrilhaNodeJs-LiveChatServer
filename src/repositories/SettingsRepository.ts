3c4cebca9c0a90ee40ba41679b281f397bd90805
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2021-04-20 18:21:58 -0300

    second day

---
+++ src/repositories/SettingsRepository.ts
@@ -0,0 +1,7 @@
+import { EntityRepository, getCustomRepository, Repository } from 'typeorm';
+import Setting from '../entities/Setting';
+
+@EntityRepository(Setting)
+class SettingsRepository extends Repository<Setting> {}
+
+export default SettingsRepository;
